68dc624b5d1ec8cf06afe0f544628931f9a5471d
BrightDaniel <ivquestservices@gmail.com>
2022-06-28 13:36:41 -0700

    betty documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ *  main -Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art"\n);
