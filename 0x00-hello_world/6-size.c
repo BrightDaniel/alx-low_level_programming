68e7039867cc3dfc9ac108e081dccc411c3159cc
BrightDaniel <ivquestservices@gmail.com>
2022-06-28 14:06:48 -0700

    small fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ *  main - Entry point
+ *   
+ *  Return: Always 0 (Success)
+ *    
+ */
 int main(void)
 {
 		int a;
