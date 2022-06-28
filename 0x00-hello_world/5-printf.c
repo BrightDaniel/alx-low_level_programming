7fa80d3770f0bfe2904648e8d760a5dec2b0244c
BrightDaniel <ivquestservices@gmail.com>
2022-06-28 13:39:53 -0700

    betty doc

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art"\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
