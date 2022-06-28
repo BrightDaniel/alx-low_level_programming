c56652eb623133e1621c153b4a3762bfa329d153
BrightDaniel <ivquestservices@gmail.com>
2022-06-28 14:11:03 -0700

    small fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 /**
- *  * main - Entry point
- *   * Return: Always 1 (Success)
- *    */
+ *  main - Entry point
+ *
+ *  Return: Always 1 (Success)
+ *
+ */
 int main(void)
 {
-		write(STDOUT_FILENO, "and that piece of art is useful
-		\" - Dora Korpar, 2015-10-19\n", 59);
+		write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 		return (1);
 }
