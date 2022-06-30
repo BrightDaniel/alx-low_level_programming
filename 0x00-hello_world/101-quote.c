f14cf6e2545406b3b42e7c48bc426911e091a3e0
BrightDaniel <ivquestservices@gmail.com>
2022-06-30 03:12:48 -0700

    small fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,12 @@
-/*
- *  * File: 101-quote.c
- *   * Auth: Brennan D Baraban
- *    */
-
+#include <stdio.h>
 #include <unistd.h>
-
 /**
- *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *   *                followed by a new line, to standard error.
- *    *
- *     * Return: Always 1.
- *      */
+ *  main - Prints out the last part of a quote in the standard error.
+ *  Return: 1 if success.
+ */
 int main(void)
 {
-		write(2,
-					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-					      	      59);
-
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 			return (1);
 }
+
