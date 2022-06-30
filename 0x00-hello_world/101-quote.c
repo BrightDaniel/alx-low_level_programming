c6dd1658c04d980d017fb2595dc82a1306b1f9a4
BrightDaniel <ivquestservices@gmail.com>
2022-06-30 02:56:19 -0700

    small fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-		write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+		write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
 			return (1);
 }
 
