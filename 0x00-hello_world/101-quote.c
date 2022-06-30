c05cb709e8617af102d96ea46aae80ec08a5529f
BrightDaniel <ivquestservices@gmail.com>
2022-06-30 02:54:29 -0700

    small fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
- *  main - Entry point
- *
- *  Return: Always 1 (Success)
- *
+*  main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
  */
 int main(void)
 {
-		write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+		write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+			return (1);
 }
+
