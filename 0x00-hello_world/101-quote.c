483f22e94aa4a26b84b6e30cde23f26284a912a9
BrightDaniel <ivquestservices@gmail.com>
2022-06-30 03:20:20 -0700

    small fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,1 @@
-#include <stdio.h>
-#include <unistd.h>
-/**
- *  main - Prints out the last part of a quote in the standard error.
- *  Return: 1 if success.
- */
-int main(void)
-{
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
-			return (1);
-}
 
