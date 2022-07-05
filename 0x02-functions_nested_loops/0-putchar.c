22cbb856e2016d8e32a432712c0c62047f2b44e7
Bright Daniel <ivquestservices@gmail.com>
2022-07-05 18:45:50 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,24 +3,6 @@
 /**
  * main - Print _putchar
  * Return: Always 0
- * Originally, the header file i used in main.h was
- * int _putchar (char *)
- * this is because i wanted my output to be
- * a string of _putchar and i wont type so much
- * and so my original description was
- *
- * char *x;
- *
- * x = "_putchar";
- * printf("%s\n", x);
- *
- * and it worked
- * but on getting to Task 1, i realised
- * i still need to use the function _putchar and
- * it can only print out a char
- * so my code had to be modified to this...
- *
- * by HilarySmartt
  */
 int main(void)
 {
