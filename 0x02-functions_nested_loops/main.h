dc5a67e671dc26b05c215447c6023eee1d30022e
BrightDaniel <ivquestservices@gmail.com>
2022-07-05 04:46:04 -0700

    main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef  MAIN_H
+#define  MAIN_H
+
+int _putchar (char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif	/*  MAIN_H *‎
