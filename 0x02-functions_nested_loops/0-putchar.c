be3fc53a708e0528b3855c455a53631f6f30b717
BrightDaniel <ivquestservices@gmail.com>
2022-07-05 04:17:56 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Prints "_putchar" followed by a new line.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
