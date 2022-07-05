855fbfb61aba2e6b24b7252ca6bd1cad9201e69e
Bright Daniel <ivquestservices@gmail.com>
2022-07-05 18:29:20 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
-#ifndef  MAIN_H
-#define  MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -15,4 +16,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif	
+#endif
