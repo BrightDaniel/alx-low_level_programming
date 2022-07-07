b5f1c4fcbcbbf97cf07f35597d8cdcd4beb86582
Bright Daniel <ivquestservices@gmail.com>
2022-07-07 17:27:59 +0100

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/#include "main.h"/**  * print_diagonal - printing lines diagonal  * @n: input variable  *  * Return: void  */7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - printing lines diagonal
+ * @n: input variable
+ *
+ * Return: void
+ */
+
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+
+}
