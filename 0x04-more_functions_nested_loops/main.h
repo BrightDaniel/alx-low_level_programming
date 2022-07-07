11ce5d0ad166b1c6e9dd93a98f568bf5bb5e54bd
Bright Daniel <ivquestservices@gmail.com>
2022-07-07 16:18:14 +0100

    main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+
+#endif 
+/* 
+Bwave ICT 
+*/
