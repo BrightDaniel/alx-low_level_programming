58782163fd9096ba97b193dc17a3ec725cbd994f
Bright Daniel <ivquestservices@gmail.com>
2022-07-07 17:26:16 +0100

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - nmbr fct
+ * Bwave ICT file
+ * Return: no return
+ */
+
+void more_numbers(void)
+{
+
+int i, j;
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+			_putchar('\n');
+		}
+}
