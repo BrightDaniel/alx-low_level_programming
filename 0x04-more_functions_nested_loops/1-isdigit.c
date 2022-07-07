c326638c5fcdb7eca5f06960aff3345e60d56904
BrightDaniel <ivquestservices@gmail.com>
2022-07-07 08:45:09 -0700

    Task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -6,7 +5,7 @@
 * @c: char to check
 * Return:  0 or 1
 **/
-int _isdigit(int c);
+int _isdigit(int c)
 {
 	if (c >= 0 && c <= 9)
 		return (1);
