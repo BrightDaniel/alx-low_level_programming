f6e7a9b7d058c0078df1d4bc7dfdb43696a34d77
Bright Daniel <ivquestservices@gmail.com>
2022-07-12 15:11:13 +0100

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "mainh"
+/**
+ * _puts - prints a string followed by a new line
+ *
+ * @str: char to check
+ * Description: This will print a string with a new line to stdout
+ *
+ * Return: 0 is success
+ */
+void _puts(char *str)
+{
+	int string;
+
+	for (string = 0; *(str + string) != '\0'; string++)
+		_putchar(*(str + string));
+	_putchar('\n');
+}
