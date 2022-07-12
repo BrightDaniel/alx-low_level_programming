a21c9a3e0fbc7cbfc4ccafefe790a5b8026afd01
Bright Daniel <ivquestservices@gmail.com>
2022-07-12 15:37:47 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,5 +11,8 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int _putchar(char c);
+
 
 #endif /*Bwave Bright*/
