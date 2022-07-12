2cb7d1e1b4f78edd5c0132f3ce18a538c27ca4b7
Bright Daniel <ivquestservices@gmail.com>
2022-07-12 15:31:29 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,21 @@
 #include "main.h"
+
 /**
- * print_rev - prints a string in reverse
- * Owned by Bwave
- * @s: int to check
- * Description: this will print a strin in reverse
- * Return: 0 is success
- */
+* print_rev -> printing a string in reverse
+* @s: the string to be printed in rev
+*/
+
 void print_rev(char *s)
 {
-	int string = 0;
+	int i, n;
 
-	for (string = 0; s[string] != '\0'; string++)
-		;
-	for (string--; string >= 0; string--)
-		_putchar(s[string]);
+	n = 0;
+	while (s[n] != '\0')
+		n++;
 
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
 	_putchar('\n');
 }
