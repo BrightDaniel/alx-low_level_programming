801ea47b549772d97143308f5cb53506d4084615
Bright Daniel <ivquestservices@gmail.com>
2022-07-12 15:29:05 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,19 @@
-#include "main.h"
+nclude "main.h"
 
 /**
-* _puts -> this is a function that puts
-* @str: a param to _puts function
-*/
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of the string
+ */
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		_putchar(*str);
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
