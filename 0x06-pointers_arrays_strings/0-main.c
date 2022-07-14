1bc049811f817cecb9ac25e4899b51d6bd8e4f5a
Bright Daniel <ivquestservices@gmail.com>
2022-07-14 14:04:27 +0100

    Create 0-main.c

---
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
+}
