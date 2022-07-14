812c4568f979c9edb530f6580a4ffd0c2de10160
Bright Daniel <ivquestservices@gmail.com>
2022-07-14 16:14:32 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,15 +1,6 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
-* _strncpy - copy a string
-* @dest:char
-* @src:char
-* @n:int
-* Return:char
-*/
-
-=======
  *_strncpy - function that copies a string
  *
  * @src: The source of strings
@@ -18,18 +9,11 @@
  *
  * Return: pointer to the resulting string dest
  */
->>>>>>> 9c16e98a580cc5bbbeb8999bc1e08d58eb520340
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+int i;
 
-<<<<<<< HEAD
-	i = 0;
-	
-	while (i < n && *(src + i))
-=======
 	for (i = 0; i < n && *(src + i); i++)
->>>>>>> 9c16e98a580cc5bbbeb8999bc1e08d58eb520340
 	{
 		*(dest + i) = *(src + i);
 	}
