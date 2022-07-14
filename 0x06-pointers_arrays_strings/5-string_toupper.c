53413e7fbf2aa331cacac9add398b7f09c3de0ff
Bright Daniel <ivquestservices@gmail.com>
2022-07-14 16:33:09 +0100

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - change lowercase to uppercase
+ * @s:string
+ * Return:char
+ */
+char *string_toupper(char *s)
+{
+
+	int i;
+
+i = 0;
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+		i++;
+	}
+	return (s);
+}
+/*Bwave ICT */
