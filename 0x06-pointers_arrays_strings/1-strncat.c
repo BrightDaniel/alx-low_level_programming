f2e563e43ad9f4af90f7532432919238f87ca1c3
Bright Daniel <ivquestservices@gmail.com>
2022-07-14 13:43:22 +0100

    Task 0 Done
    The first task, but in programming, we start counting from 0.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,27 @@
+#include "main.h"
 
+/**
+ * _strcat - concat 2 string
+ * @dest:char
+ * @src:char
+ * Return:char
+ * Owned by Bwave
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *s = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
+}
