6d573eef851ff2717bfd5714263516f98d930f10
Bright Daniel <ivquestservices@gmail.com>
2022-07-14 13:45:22 +0100

    main.h prototype
    All my prototypes in here

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *s);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
