8cc43531731e34d67f42d7a0642b76205af067c3
Bright Daniel <ivquestservices@gmail.com>
2022-07-20 11:39:24 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != 0)
-	{
-		_putchar(*s);
-		
-	}
-		_puts_recursion(s + 1);
+	if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
 	
 }
