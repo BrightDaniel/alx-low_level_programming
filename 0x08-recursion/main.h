4f961f991abdf11d223e9c196d43f33bade9255d
BrightDaniel <ivquestservices@gmail.com>
2022-07-20 02:45:49 -0700

    task 0

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int factorial(int n);
+#endif /*Bwave*/
