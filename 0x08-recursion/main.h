2011ed51986b4cff04297d711150cb1b7bee41f1
Bright Daniel <ivquestservices@gmail.com>
2022-07-20 11:08:48 +0100

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,14 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
-int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
-int _putchar(char c);
-#include <stdio.h>
+int factorial(int n);
 #endif /*Bwave*/
