43baf0db427ce0476f82376cbb22e1847128382b
BrightDaniel <ivquestservices@gmail.com>
2022-07-20 04:51:18 -0700

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: string
+ * Return 0;
+*/
 
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_print_rev_recursion(s - 1);
+	}
+}
