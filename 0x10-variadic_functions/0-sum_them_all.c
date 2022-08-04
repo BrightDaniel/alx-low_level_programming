be4b79f0a7cbcf36b1c03a070a0a9e79c07faa70
Bright Daniel <ivquestservices@gmail.com>
2022-08-04 08:59:26 -0700

    Task 0 changes

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,10 +8,9 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list param;
-	int i, sum;
+	unsigned int i; int sum = 0;
 
 	va_start(param, n); /*Macro to intialize the argument in the parameter*/
-	sum = 0;
 	for (i = 0; i < n; i++)
 		sum += va_arg(param, n);  /*Get the next parameter in the function*/
 	va_end(param); /*Macro to end the var fun*/
