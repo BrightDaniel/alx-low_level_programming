712d61c7289b138f46f66c10d956b86ec7b87aa0
Bright Daniel <ivquestservices@gmail.com>
2022-08-04 18:03:57 +0100

    Updated

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -21,3 +21,4 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+#endif
